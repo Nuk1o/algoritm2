200d437bbea2b041c03094be056eb863f86a576b
Nukio <NukiXm@yandex.ru>
2023-01-11 19:30:31 +0300

    Full UI and 2 scripts
    Пересобрал весь интерфейс и сделал 2 скрипта для открытия вопроса и закрытие программы. Приложение полностью на русском

---
+++ Algoritm2/Assets/Scripts/Core/Core.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core : MonoBehaviour
+{
+    /*
+     * Скрипт для работы с бд а так же для дальнейших манипуляций
+     */
+}
---
+++ Algoritm2/Assets/Scripts/Menu/Quest_menu.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quest_menu : MonoBehaviour
+{
+    [SerializeField] private GameObject _menu_quest;//Меню с вопросом
+    [SerializeField] private TMP_Text _quest_txt;//Текст вопроса на отобразившимся меню
+    [SerializeField] private string _Enter_txt;//Введите текст вопроса
+    [SerializeField] private Button _btn_cancle, _btn_ok;//Кнопки да и отмена
+    [Space]
+    [SerializeField] private GameObject _menu1;//Меню на котором мы находимся
+    [SerializeField] private GameObject _menu2;//Меню которое нужно включить
+
+    private void Start()
+    {
+        _menu_quest.SetActive(false);
+    }
+
+    public void quest()
+    {
+        _menu_quest.SetActive(true);
+        _quest_txt.text = _Enter_txt;
+        
+        _btn_ok.onClick.AddListener(delegate { Start_quest(_menu1,_menu2,_menu_quest); });
+        _btn_cancle.onClick.AddListener(delegate { Close_quest(_menu_quest); });
+    }
+
+    private void Start_quest(GameObject _menu1,GameObject _menu2,GameObject _menu_quest)
+    {
+        _menu1.SetActive(false);
+        _menu2.SetActive(true);
+        _menu_quest.SetActive(false);
+    }
+
+    private void Close_quest(GameObject _menu_quest)
+    {
+        _menu_quest.SetActive(false);
+    }
+}
